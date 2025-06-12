0fbe2a884a73436a992e5f8c7f14b6eaa653364b
Railan Santos <railanpdssantos@gmail.com>
2025-06-12 12:54:23 -0300

    Primeiro commit - projeto loja virtual

---
+++ src/main/java/com/dev/backend/controller/Hello.java
@@ -0,0 +1,17 @@
+package com.dev.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+
+    @GetMapping
+    public String hello() {
+        return "Vai tomar no cu " + new Date();
+    }
+}
